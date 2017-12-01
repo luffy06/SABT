c23e953b3d1ee0efc17d8888af1c25210fe28a6f
wushangyu <534427411@qq.com>
2017-12-01 19:15:14 +0800

    加入清华api

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from QcloudApi.qcloudapi import QcloudApi
 from aip import AipNlp
 import jieba
+import thulac
 import fileutil
 import json
 
@@ -50,9 +51,9 @@ class TencentWenZhi(object):
       print("error:" + text + " in LexicalCheck\nMessage:" + res['message'])
       return text
 
-  def cutWord(self, text, times):
+  def cutWord(self, text, times, errormessage):
     if times > 5:
-      print("error:" + text + " in LexicalAnalysis\nMessage: TimeOut")
+      print("error:" + text + " in LexicalAnalysis\nMessage: " + errormessage)
       jb = JieBa()
       return jb.cutWord(text)
 
@@ -70,7 +71,7 @@ class TencentWenZhi(object):
       return result
     else:
       # print("error:" + text + "in LexicalAnalysis\nMessage:" + res['message'])
-      return self.cutWord(text, times + 1)
+      return self.cutWord(text, times + 1, res['message'])
 
 
   def cutWordByCSVFile(self, filenamein, filenameout):
@@ -79,7 +80,7 @@ class TencentWenZhi(object):
     result = ""
     for rd in rawdata:
       text = rd.text
-      res = self.cutWord(text, 0)
+      res = self.cutWord(text, 0, "")
       for r in res:
         result = result + r + " "
       result = result + "\n"
@@ -115,20 +116,20 @@ class BaiDuNlp(object):
     self.SECRET_KEY = '3os02bOi9hxZC9775MbKVcYo4BP7GTSm'
     self.client = AipNlp(self.APP_ID, self.API_KEY, self.SECRET_KEY)
 
-  def cutWord(self, text, times):
+  def cutWord(self, text, times, errormessage):
     if times > 5:
-      print("error:" + text + " in BaiDuNlp\nMessage: TimeOut")
+      print("error:" + text + " in BaiDuNlp\nMessage: " + errormessage)
       jb = JieBa()
       return jb.cutWord(text)
 
     result = []
-    res = self.client.dnnlm(text.encode('GBK'))
+    res = self.client.dnnlm(text)
     if 'error_code' not in res:
       for r in res['items']:
         result.append(r['word'])
       return result
     else:
-      return self.cutWord(text, times + 1)
+      return self.cutWord(text, times + 1, res['error_msg'])
 
   def cutWordByCSVFile(self, filenamein, filenameout):
     rawdata = fileutil.readFileFromCSV(filenamein)
@@ -136,21 +137,50 @@ class BaiDuNlp(object):
     result = ""
     for rd in rawdata:
       text = rd.text
-      res = self.cutWord(text, 0)
+      res = self.cutWord(text, 0, "")
       for r in res:
         result = result + r + " "
       result = result + "\n"
     fileutil.writeFile(filenameout, result)
 
+class ThuLac(object):
+  """docstring for ThuLac"""
+  def __init__(self):
+    super(ThuLac, self).__init__()
+    self.thu = thulac.thulac()
+    
+  def cutWord(self, text):
+    result = self.thu.cut(text)
+    return result
 
-def test():
-  # jb = JieBa()
-  # JieBa.cutWordByCSVFile("./data/test_semi.csv", "./data/test_cutted.out")
-  # twz = TencentWenZhi()
-  # twz.cutWordByCSVFile("./data/test_semi.csv", "./data/test_cutted.out")
-  an = BaiDuNlp()
-  an.cutWordByCSVFile("./data/test_semi.csv", "./data/test_cutted.out")
+  def cutWordByCSVFile(self, filenamein, filenameout):
+    rawdata = fileutil.readFileFromCSV(filenamein)
+    fileutil.deleteFileIfExist(filenameout)
+    result = ""
+    for rd in rawdata:
+      text = rd.text
+      res = self.cutWord(text)
+      for r in res:
+        result = result + r[0] + " "
+      result = result + "\n"
+    fileutil.writeFile(filenameout, result)
+
+    
+
+def test(method, filenamein, filenameout):
+  if method == 1:
+    jb = JieBa()
+    jb.cutWordByCSVFile(filenamein, filenameout)
+  elif method == 2:
+    twz = TencentWenZhi()
+    twz.cutWordByCSVFile(filenamein, filenameout)
+  elif method == 3:
+    an = BaiDuNlp()
+    an.cutWordByCSVFile(filenamein, filenameout)
+  elif method == 4:
+    th = ThuLac()
+    th.cutWordByCSVFile(filenamein, filenameout)
 
 if __name__ == '__main__':
-  test()
+  test(4, "./data/test_semi.csv", "./data/test_cutted.out")
 
